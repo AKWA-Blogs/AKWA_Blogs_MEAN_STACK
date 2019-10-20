466321628111f871943d44958258c8d1986d428e
Abdulaziz Saud <azoz-saud@live.com>
2019-10-20 11:38:04 +0300

    added database collection

--- server.js
+++ server.js
@@ -6,10 +6,26 @@ app.use(express.static( __dirname + '/public/dist/public' ));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-mongoose.connect("mongodb://localhost/", {useNewUrlParser: true, useUnifiedTopology: true});
-require("./server/config/mongoose.js");
 
-require("./server/config/routes.js")(app);
+mongoose.connect("mongodb+srv://AKWA-SITE:codingdojo2019@akwa-h0aa7.mongodb.net/akwa_db", {useNewUrlParser: true, useUnifiedTopology: true});
+
+let db = mongoose.connection;
+
+
+// Check Database connection
+db.once('open', function () {
+    console.log("Connected to mongoDB. ");
+});
+
+
+// check Database errors
+db.on('error', function (err) {
+    console.log(err);
+});
+
+// require("./server/config/routes.js")(app);
+
+
 
 app.listen(8000, function(){
     console.log("Listening on port: 8000");
--- server/config/mongoose.js
+++
@@ -1,9 +0,0 @@
-var fs = require("fs");
-var path = require("path");
-
-var models_path = path.join(__dirname, "./../models");
-
-fs.readdirSync(models_path).forEach(function(file){
-	if(file.indexOf(".js") >= 0)
-		require(models_path+"/"+file);
-});
