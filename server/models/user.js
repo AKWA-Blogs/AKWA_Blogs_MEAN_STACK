df59c501c6b2b05a3fae375dd40f03e3290fd732
Abdulaziz Saud <azoz-saud@live.com>
2019-10-21 09:48:28 +0300

    Update user.js

--- server/models/user.js
+++ server/models/user.js
@@ -5,7 +5,7 @@ var Article = require('./article');
 var UserSchema = new mongoose.Schema({
     first_name: { type: String, required: true },
     last_name: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true},
     password: { type: String, required: true },
     // channels:[ChannelSchema],
     likes: [Article],
