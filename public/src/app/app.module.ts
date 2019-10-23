7b5510cbff7d3d9f447db5e34d4ae2f9ab994a73
khalidzhn <43253007+khalidzhn@users.noreply.github.com>
2019-10-24 01:20:29 +0300

    explore

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -8,17 +8,30 @@ import { HttpClientModule } from '@angular/common/http';
 import { HttpService } from './http.service';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { ExploreComponent } from './explore/explore.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatTabsModule} from '@angular/material/tabs';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import { ExplorerComponent } from './explorer/explorer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExploreComponent,
+    ExplorerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    MatSliderModule,
+    MatCardModule,
+    MatTooltipModule,
+    MatTabsModule,
+    
+
     BrowserAnimationsModule
   ],
   providers: [HttpService],
