0f96a328037c6b401111c878ea055ad442643831
Wafi AlShammari <wafi543@outlook.sa>
2019-10-24 10:46:09 +0300

    finish register functionality

--- angular-app/src/app/pages/register/register.component.ts
+++ angular-app/src/app/pages/register/register.component.ts
@@ -1,20 +1,69 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, ElementRef, OnDestroy } from '@angular/core';
+import { FormControl, FormGroupDirective, NgForm, Validators, FormGroup } from '@angular/forms';
+import { HttpService } from '../../http.service';
+import * as jwt_decode from 'jwt-decode';
+import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-register-cmp',
-    templateUrl: './register.component.html'
+  selector: 'app-register-cmp',
+  templateUrl: './register.component.html'
 })
 
 export class RegisterComponent implements OnInit, OnDestroy {
-    test: Date = new Date();
-    ngOnInit() {
-      const body = document.getElementsByTagName('body')[0];
-      body.classList.add('register-page');
-      body.classList.add('off-canvas-sidebar');
-    }
-    ngOnDestroy(){
-      const body = document.getElementsByTagName('body')[0];
-      body.classList.remove('register-page');
-      body.classList.remove('off-canvas-sidebar');
-    }
+  test: Date = new Date();
+  private toggleButton: any;
+  private sidebarVisible: boolean;
+  private nativeElement: Node;
+
+  fnameFormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3)
+  ]);
+  lnameFormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3)
+  ]);
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  passFormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(8)
+  ]);
+  confirmFormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(8)
+  ]);
+
+  constructor(private element: ElementRef, private _httpService: HttpService, private router: Router) {
+    this.nativeElement = element.nativeElement;
+    this.sidebarVisible = false;
+  }
+
+  ngOnInit() {
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.add('register-page');
+    body.classList.add('off-canvas-sidebar');
+  }
+  ngOnDestroy() {
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.remove('register-page');
+    body.classList.remove('off-canvas-sidebar');
+  }
+
+  newUser = { first_name: "", last_name: "", email: "", password: "" }
+
+  onSubmit() {
+    if (this.passFormControl.value != this.confirmFormControl.value) {return}
+    console.log("registering...")
+    let observable = this._httpService.postSignUp(this.newUser)
+    observable.subscribe(data => {
+      console.log('done! ', data['token'])
+      var user = jwt_decode(data['token'])
+      localStorage.setItem('id', user.id)
+      console.log('setting "id" in local: ' + localStorage.getItem('id'))
+      this.router.navigate(['/'])
+    })
+  }
 }
