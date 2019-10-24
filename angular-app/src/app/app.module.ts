83877376524b5b4e9041589e200d393694052034
Wafi AlShammari <wafi543@outlook.sa>
2019-10-24 20:51:56 +0300

    add profile component

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -52,6 +52,7 @@ import { AdminLayoutComponent } from './layouts/admin/admin-layout.component';
 import { AuthLayoutComponent } from './layouts/auth/auth-layout.component';
 
 import { AppRoutes } from './app.routing';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   exports: [
@@ -86,7 +87,8 @@ import { AppRoutes } from './app.routing';
     MatTabsModule,
     MatToolbarModule,
     MatTooltipModule
-  ]
+  ],
+  declarations: [ProfileComponent]
 })
 export class MaterialModule { }
 
