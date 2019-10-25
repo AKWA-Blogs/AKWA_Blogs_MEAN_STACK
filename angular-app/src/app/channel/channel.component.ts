ec0a5c87c5dfcd67eb385e9a71563f7c7b90b0fe
Abdulaziz Saud <azoz-saud@live.com>
2019-10-25 12:38:56 +0300

    update channel component

--- angular-app/src/app/channel/channel.component.ts
+++ angular-app/src/app/channel/channel.component.ts
@@ -1,8 +1,8 @@
-// IMPORTANT: this is a plugin which requires jQuery for initialisation and data manipulation
-
 import { Component, OnInit } from '@angular/core';
 import swal from 'sweetalert2';
 import PerfectScrollbar from 'perfect-scrollbar';
+import { HttpService } from '../http.service';
+
 
 declare const $: any;
 
@@ -12,8 +12,18 @@ declare const $: any;
 })
 
 export class ChannelComponent implements OnInit {
-    ngOnInit() {
+    channel = {}
+    constructor(private _httpService: HttpService) {
 
-  
+    }
+    ngOnInit() {
+        this.getUserChannelFromService();
+    }
+    getUserChannelFromService() {
+        let observable = this._httpService.getUserChannel(localStorage.getItem('id'));
+        observable.subscribe(channel => {
+            console.log("Got subbed channel!!", channel);
+            this.channel = channel;
+        });
     }
 }
