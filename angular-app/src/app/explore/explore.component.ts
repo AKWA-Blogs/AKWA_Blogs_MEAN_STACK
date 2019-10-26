9ba3253b8643e195912bb090ec124a1afd7967a5
Abdulaziz Saud <azoz-saud@live.com>
2019-10-26 13:55:28 +0300

    Update explore.component.ts

--- angular-app/src/app/explore/explore.component.ts
+++ angular-app/src/app/explore/explore.component.ts
@@ -28,9 +28,9 @@ export class ExploreComponent implements OnInit {
   tags = {}
 
   public items = [
-    {display: 'Pizza', value: 1},
-    {display: 'Pasta', value: 2},
-    {display: 'Parmesan', value: 3},
+    { display: 'Pizza', value: 1 },
+    { display: 'Pasta', value: 2 },
+    { display: 'Parmesan', value: 3 },
   ];
   constructor(private _httpService: HttpService) {
 
@@ -61,31 +61,17 @@ export class ExploreComponent implements OnInit {
     observable.subscribe(data => {
       console.log("Got our Articles!!", data);
       this.articles = data
-
-
-      /*       for(var x in data ) {
-              if (data[x]['_id']=='5daec7cd1e8d9a78b1a355e6'){
-                this.selectedArticles = data[x];
-                console.log(x,"Go________fvdse____________!!", this.selectedArticles);
-      
-              }
-              else{
-                this.articles[x]= data[x]
-      
-              }
-            }
-            console.log(x,"Go____________________!!", this.articles);
-       */
     });
   };
 
   getChannelsFromService() {
-        let observable = this._httpService.expChannel('5daffc8c44476296b93ab61a');
+    let observable = this._httpService.expChannel('5daffc8c44476296b93ab61a');
     console.log("in expChannels")
 
     observable.subscribe(data => {
       console.log("Got our Channels!!", data);
       this.channels = data;
 
-  });
-}}
+    });
+  }
+}
