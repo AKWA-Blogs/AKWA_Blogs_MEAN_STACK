b285baa01e6ce23a80563dbabb22951c15bbb762
Abdulaziz Saud <azoz-saud@live.com>
2019-10-26 13:43:27 +0300

    Update explore.component.ts

--- angular-app/src/app/explore/explore.component.ts
+++ angular-app/src/app/explore/explore.component.ts
@@ -39,21 +39,11 @@ export class ExploreComponent implements OnInit {
   doubleSlider = [20, 60];
 
   regularItems = ['Pizza', 'Pasta', 'Parmesan'];
-  touch: boolean;
 
-  selectedValue: string;
-  currentCity: string[];
+
 
   selectTheme = 'primary';
-  cities = [
-    { value: 'paris-0', viewValue: 'Paris' },
-    { value: 'miami-1', viewValue: 'Miami' },
-    { value: 'bucharest-2', viewValue: 'Bucharest' },
-    { value: 'new-york-3', viewValue: 'New York' },
-    { value: 'london-4', viewValue: 'London' },
-    { value: 'barcelona-5', viewValue: 'Barcelona' },
-    { value: 'moscow-6', viewValue: 'Moscow' },
-  ];
+
 
   ngOnInit() {
 
