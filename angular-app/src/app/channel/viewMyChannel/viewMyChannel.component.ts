02dc01bec0bf3b925cef5acb6110cb5d568ae1c1
abdulrabbt <abdulrab.bt@hotmail.com>
2019-10-27 09:45:07 +0300

    edit tags

--- angular-app/src/app/channel/viewMyChannel/viewMyChannel.component.ts
+++ angular-app/src/app/channel/viewMyChannel/viewMyChannel.component.ts
@@ -18,7 +18,8 @@ export class ViewMyChannelComponent implements OnInit {
   channel = {}
   articles = {}
   editArticle = {}
-  regularItems: string[] = [];
+  regularItems = [];
+  tags =[]
   showEdit: boolean = false;
   constructor(private _httpService: HttpService) { }
 
@@ -81,6 +82,20 @@ export class ViewMyChannelComponent implements OnInit {
 
   getEditArticle() {
     this.showEdit = false;
+    console.log(this.regularItems)
+
+    for (var i = 0; i < this.regularItems.length; i++) {
+      if (typeof this.regularItems[i] == "string") {
+        this.tags.push(this.regularItems[i]);
+      };
+      if (typeof this.regularItems[i] == "object") {
+        this.tags.push(this.regularItems[i].display);
+      };
+    }
+    console.log("New tags "+this.tags);
+    this.editArticle["tags"] = this.tags;
+
+    // this.editArticle.tags = this.regularItems;
     console.log('update    ', this.editArticle);
 
     let observable = this._httpService.putArticle(this.editArticle)
